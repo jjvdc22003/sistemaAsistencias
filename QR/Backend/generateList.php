78989df7401b39efe835612d90dabf266a02f44a
saulpena636 <suoerpenniya@gmail.com>
2024-08-26 16:52:55 -0600

    Generar alumnos

---
+++ QR/Backend/generateList.php
@@ -0,0 +1,134 @@
+<?php
+//composer require phpoffice/phpspreadsheet
+require 'vendor/autoload.php';
+require "BD.php";
+require_once __DIR__."/Controllers/alumnosController.php";
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+class generateList {
+    public function generar($fecha){
+      $spreadsheet = new Spreadsheet();
+      $sheet = $spreadsheet->getActiveSheet();
+
+      $conexion= new Conexion;
+
+      $sqlListar = "SELECT * FROM asistencias, alumnos WHERE asistencias.fecha='".$fecha."' AND alumnos.matricula = asistencias.matricula_alumno ORDER BY grupo, nombre";
+      $lista=$conexion->ejecutar($sqlListar);
+      $resultado=$lista->fetchALL();
+
+      $sheet->setCellValue('A1', 'Lista de asistencia del dia '.$fecha);
+      $sheet->mergeCells('A1:F1');
+      $sheet->getStyle('A1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+      // Escribir datos en la hoja de cálculo
+      $sheet->setCellValue('A2', 'Matricula');
+      $sheet->setCellValue('B2', 'Nombre');
+      $sheet->setCellValue('C2', 'Carrera');
+      $sheet->setCellValue('D2', 'Grupo');
+      $sheet->setCellValue('E2','Hora de entrada');
+      $sheet->setCellValue('F2','Hora de salida');
+
+      $row = 3;
+
+      foreach ($resultado as $key) {
+          $sheet -> setCellValue('A'.$row, $key['matricula']);
+          $sheet -> setCellValue('B'.$row, $key['nombre']);
+          $sheet -> setCellValue('C'.$row, $key['carrera']);
+          $sheet -> setCellValue('D'.$row, $key['grupo']);
+          $sheet -> setCellValue('E'.$row, $key['hora_entrada']);
+          $sheet -> setCellValue('F'.$row, $key['hora_salida']);
+          $row ++;
+      }
+
+      $sheet->getColumnDimension('A')->setAutoSize(true);
+      $sheet->getColumnDimension('B')->setAutoSize(true);
+      $sheet->getColumnDimension('C')->setAutoSize(true);
+      $sheet->getColumnDimension('D')->setAutoSize(true);
+      $sheet->getColumnDimension('E')->setAutoSize(true);
+      $sheet->getColumnDimension('F')->setAutoSize(true);
+
+      // Guardar la hoja de cálculo en un archivo
+      $ruta = __DIR__.'/../assets/listas/';
+      if (!is_dir($ruta)) {
+          mkdir($ruta, 0777, true);
+      }
+      $writer = new Xlsx($spreadsheet);
+      $writer->save($ruta.'asistencia'.$fecha.'.xlsx'); 
+
+      $rutaArchivo = $ruta.'asistencia'.$fecha.'.xlsx';
+      if (file_exists($rutaArchivo)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=' . basename($rutaArchivo));
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($rutaArchivo));
+        readfile($rutaArchivo);
+        exit;
+    } else {
+        echo "El archivo no existe.";
+    }
+  }
+
+  public function generateAlumnos($ruta){
+    $spreadsheet = IOFactory::load($ruta);
+    $sheet = $spreadsheet->getActiveSheet();
+    $a = 2;
+    $a1 = 0;
+    
+    do {
+      $matricula = $sheet->getCell('A'.$a)->getValue();
+      $nombre = $sheet->getCell('B'.$a)->getValue();
+      $sexo = $sheet->getCell('C'.$a)->getValue();
+      $correo = $sheet->getCell('D'.$a)->getValue();
+      $carrera = $sheet->getCell('E'.$a)->getValue();
+      $grupo = $sheet->getCell('F'.$a)->getValue();
+
+      $alumno = array(
+        'matricula' => (isset($matricula)) ? $matricula : "",
+        'nombre' => (isset($nombre)) ? $nombre : "",
+        'sexo' => (isset($sexo)) ? $sexo : "",
+        'correo' => (isset($correo)) ? $correo : "",
+        'carrera' => (isset($carrera)) ? $carrera : "",
+        'grupo' => (isset($grupo)) ? $grupo : ""
+    );
+    $a = $a+1;
+    $alumnoController = new alumno;
+        $buscar = $alumnoController->listOneMatricula($matricula);
+        $buscarCorreo = $alumnoController->listOnePorCorreo($correo);
+        if($buscar['status'] == 200){
+            if(!empty($buscar['alumno'])){
+              $al2 = $alumnoController->update($buscar['alumno'][0]['id'],$alumno);
+              $a1++;
+            } elseif($buscarCorreo['status'] == 200) {
+
+                if(!empty($buscarCorreo['cuenta'])){
+
+                } else{
+                    //Agregar
+                    $agregar = $alumnoController->create($alumno);
+                    if($agregar['status'] == 200){
+                      $a1++;
+                    } else {
+                        return -1;
+                    }
+                }
+            } else {
+              return -1;
+            }
+        } else {
+          return -1;
+        }
+        $matricula = $sheet->getCell('A'.$a)->getValue();
+    } while ($matricula!="");
+    return $a1;
+  }
+}
+
+
+?>
--- QR/Backend/vendor/composer/autoload_static.php
+++ QR/Backend/vendor/composer/autoload_static.php
@@ -6,7 +6,32 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit5726834464b54df5c26cde494ec31af2
 {
+    public static $files = array (
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        '2cffec82183ee1cea088009cef9a6fc3' => __DIR__ . '/..' . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
+        'Z' => 
+        array (
+            'ZipStream\\' => 10,
+        ),
+        'S' => 
+        array (
+            'Symfony\\Polyfill\\Mbstring\\' => 26,
+        ),
+        'P' => 
+        array (
+            'Psr\\SimpleCache\\' => 16,
+            'Psr\\Http\\Message\\' => 17,
+            'Psr\\Http\\Client\\' => 16,
+            'PhpOffice\\PhpSpreadsheet\\' => 25,
+        ),
+        'M' => 
+        array (
+            'MyCLabs\\Enum\\' => 13,
+            'Matrix\\' => 7,
+        ),
         'E' => 
         array (
             'Endroid\\QrCode\\' => 15,
@@ -15,6 +40,10 @@ class ComposerStaticInit5726834464b54df5c26cde494ec31af2
         array (
             'DASPRiD\\Enum\\' => 13,
         ),
+        'C' => 
+        array (
+            'Complex\\' => 8,
+        ),
         'B' => 
         array (
             'BaconQrCode\\' => 12,
@@ -22,6 +51,39 @@ class ComposerStaticInit5726834464b54df5c26cde494ec31af2
     );
 
     public static $prefixDirsPsr4 = array (
+        'ZipStream\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/maennchen/zipstream-php/src',
+        ),
+        'Symfony\\Polyfill\\Mbstring\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
+        ),
+        'Psr\\SimpleCache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/simple-cache/src',
+        ),
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+        ),
+        'Psr\\Http\\Client\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-client/src',
+        ),
+        'PhpOffice\\PhpSpreadsheet\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpoffice/phpspreadsheet/src/PhpSpreadsheet',
+        ),
+        'MyCLabs\\Enum\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/myclabs/php-enum/src',
+        ),
+        'Matrix\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/markbaker/matrix/classes/src',
+        ),
         'Endroid\\QrCode\\' => 
         array (
             0 => __DIR__ . '/..' . '/endroid/qr-code/src',
@@ -30,14 +92,29 @@ class ComposerStaticInit5726834464b54df5c26cde494ec31af2
         array (
             0 => __DIR__ . '/..' . '/dasprid/enum/src',
         ),
+        'Complex\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/markbaker/complex/classes/src',
+        ),
         'BaconQrCode\\' => 
         array (
             0 => __DIR__ . '/..' . '/bacon/bacon-qr-code/src',
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'H' => 
+        array (
+            'HTMLPurifier' => 
+            array (
+                0 => __DIR__ . '/..' . '/ezyang/htmlpurifier/library',
+            ),
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'Stringable' => __DIR__ . '/..' . '/myclabs/php-enum/stubs/Stringable.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -45,6 +122,7 @@ class ComposerStaticInit5726834464b54df5c26cde494ec31af2
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit5726834464b54df5c26cde494ec31af2::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit5726834464b54df5c26cde494ec31af2::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit5726834464b54df5c26cde494ec31af2::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit5726834464b54df5c26cde494ec31af2::$classMap;
 
         }, null, ClassLoader::class);
